9fef9d4422963d5387512b16042f9142d5598639
JuanIgnacio006 <juangode@myuax.com>
2024-11-05 11:20:23 +0100

    Ejercicio 3 TERMINADO(EXPLICACION CODIGO)

--- Ejercicio3/ej3.cpp
+++ Ejercicio3/ej3.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void promedio()
 {
 float nota1,nota2,nota3,nota4,nota5,nota6,nota7,nota8,promedio;
-
+//PEDIMOS AL USUARIO QUE INGRESE LAS NOTAS DE LOS 8 EJERCICIOS
  cout<<"Ingrese la nota del primer ejercicio: ";cin>>nota1;
  cout<<"Ingrese la nota del segundo ejercicio: ";cin>>nota2;
  cout<<"Ingrese la nota del tercer ejercicio: ";cin>>nota3;
@@ -18,11 +18,16 @@ float nota1,nota2,nota3,nota4,nota5,nota6,nota7,nota8,promedio;
  cout<<"Ingrese la nota del sexto ejercicio: ";cin>>nota6;
  cout<<"Ingrese la nota del septimo ejercicio: ";cin>>nota7;
  cout<<"Ingrese la nota del octavo ejercicio: ";cin>>nota8;
+    //UTILIZAMOS UNA ESTRUCTURA CONDICIONAL PARA VERIFICAR QUE LAS NOTAS ESTEN ENTRE 0 Y 10
+    //SI LAS NOTAS ESTAN ENTRE 0 Y 10, CALCULAMOS EL PROMEDIO DE LAS NOTAS
 if(nota1>0 && nota1<10 && nota2>0 && nota2<10 && nota3>0 && nota3<10 && nota4>0 && nota4<10 && nota5>0 && nota5<10 && nota6>0 && nota6<10 && nota7>0 && nota7<10 && nota8>0 && nota8<10)
 {
     promedio=(nota1+nota2+nota3+nota4+nota5+nota6+nota7+nota8)/8;
     cout<<"El promedio de las notas es: "<<promedio<<endl;
 }
+    /*EN EL CASO QUE HAYA ALGUNA NOTA QUE NO ESTE ENTRE 0 Y 10,
+    SE IMPRIME QUE TODAS LAS NOTAS DEBEN ESTAR ENTRE 0 Y 10,
+    YA QUE NO SE CUMPLE LA CONDICION QUE LAS NOTAS ESTEN ENTRE 0 Y 10*/
 else
 {
     cout<<"Todas las notas deben estar entre 0 y 10"<<endl;
