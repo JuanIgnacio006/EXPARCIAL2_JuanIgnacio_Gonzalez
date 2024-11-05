a2f851e0c49583603bec7a20bc6876c65ff8abad
JuanIgnacio006 <juangode@myuax.com>
2024-11-05 11:16:00 +0100

    Ejercicio 1 TERMINADO(EXPLICACION CODIGO)

--- Ejercicio1/ej1.cpp
+++ Ejercicio1/ej1.cpp
@@ -6,16 +6,21 @@ El programa deberá analizar el número ingresado e imprimir en pantalla si el n
 using namespace std;
 void comprobacion()
 {
+    //PEDIMOS EL NUMERO ENTERO AL USUARIO
     int numero;
     cout<<"Ingrese un numero entero: ";cin>>numero;
+    //UTILIZAMOS UNA ESTRUCTURA CONDICIONAL PARA DETERMINAR SI EL NUMERO ES POSITIVO, NEGATIVO O CERO
+    //EN CASO DE QUE EL NUMERO SEA 0, SE IMPRIME QUE EL NUMERO ES CERO
     if(numero==0)
     {
         cout<<"El numero es cero"<<endl;
     }
+    //EN CASO DE QUE EL NUMERO SEA MAYOR A 0, SE IMPRIME QUE EL NUMERO ES POSITIVO
     else if(numero>0)
     {
         cout<<"El numero "<<numero<<" es positivo"<<endl;
     }
+    //EN CASO QUE EL NUMERO SEA MENOR A 0, SE IMPRIME QUE EL NUMERO ES NEGATIVO
     else
     {
         cout<<"El numero "<<numero<<" es negativo"<<endl;
